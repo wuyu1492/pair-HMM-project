de55c89c9d674dc145951184322cdcd6cf45e335
WU YU <wuyu1492@gmail.com>
2017-09-01 15:11:19 +0800

    Add files via upload

--- testmain.py
+++ testmain.py
@@ -1,6 +1,6 @@
 from read_data import *
 from getTrans import *
-import numpy as np
+from PEring2 import *
 import sys, getopt
 
 def main(argv):
@@ -19,19 +19,27 @@ def main(argv):
     print('inputfile = ', inputfile)
     print('outputfile = ', outputfile)
     data_all = readInput(inputfile)
-    #print('data_all:', len(data_all))
+    f = open(outputfile, 'w')
+    ring_size = 4
+    myring = PEring(ring_size)
     for d in range(0, len(data_all)):
         print('Processing data set', d)
+        f.write("data set %d \n" % d)
         data = data_all[d]
-        for l in range(0, len(data[0])):
-            print('base No.', l)
-            for h in range(0, len(data[5])):
-                print('haplo No.', h)
+        for l in range(len(data[0])):        # for each line in one set
+            print('base ', l)
+            f.write("base %d \n" % l)
+            mm, im, dm, mi, ii, md, dd = getTrans(data[2][l], data[3][l], data[4][l])
+            for h in range(0, len(data[5])):    # for each haplo
+                print('haplo', h, end=" ")
+                f.write("haplo %d " % h)
                 prior = getPrior(data[0][l], data[5][h], data[1][l])
-                mm, im, dm, mi, ii, md, dd = getTrans(data[2][l], data[3][l], data[4][l])
-    #prior = getPrior(data_all[0][0][0], data_all[0][5][0], data_all[0][1][0] )
-    #print('prior.shape =', prior.shape)
-    #mm, im, dm, mi, ii, md, dd = getTrans(data_all[0][2][0], data_all[0][3][0], data_all[0][4][0])
+                myring.initRing(mm[0])               # initial PE ring
+                result = myring.dp(prior, mm, im, dm, mi, ii, md, dd)
+                print(result)
+                f.write("result = %e\n" % result)
+        #break
+    f.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
